8d669aea549106f06100483bf433cfdd4de9ea78
Noah Dumba <noahdumba1@gmail.com>
2020-01-06 22:08:44 +0000

    My own equals test. WITH EMOJIS :D

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (console.log("✅✅✅ " + actual + " === " + expected));
+  if (actual !== expected)
+    return (console.log("❌❌❌ " + actual + " !== " + expected));
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Foo", "Foo");
+assertEqual(1, 1.1);
