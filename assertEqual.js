f78f26de2cee6ce13c711230026d8371aef9116b
Noah Dumba <noahdumba1@gmail.com>
2020-01-06 22:17:21 +0000

    changed output to template strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    return (console.log("✅✅✅ " + actual + " === " + expected));
+    return (console.log(`✅✅✅  ${actual} === ${expected}`));
   if (actual !== expected)
-    return (console.log("❌❌❌ " + actual + " !== " + expected));
+    return (console.log(`❌❌❌ ${actual} !== ${expected}`));
 };
 
 // TEST CODE
