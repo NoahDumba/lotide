9f974cc309af16c41f5fbd0c5bedff789f18f58a
Noah Dumba <noahdumba1@gmail.com>
2020-01-07 21:41:37 +0000

    checks if arrays are equal and displays a message to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const arraysEqAssert = function(manta, manta2) {
+  let success = `✅✅✅  [${manta}] === [${manta2}]`;
+  let failure = `❌❌❌ [${manta}] !== [${manta2}]`;
+
+  if(manta.length !== manta2.length)
+    return console.log(failure);
+
+  for (let i = 0; i < manta.length; i++) {
+    if (manta[i] !== manta2[i])
+      return console.log(failure);
+  }
+  return console.log(success)
+};
