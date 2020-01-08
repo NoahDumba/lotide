691ea0369565b4a05af4d2245c9c041d1e3c7981
Noah Dumba <noahdumba1@gmail.com>
2020-01-08 17:53:03 +0000

    counts the number of each letter in a string and returns them as letter : instance pair

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const countLetters = function(letters) {
+  let results = [];
+  const manta = letters.split('')
+
+  for (const letter of manta) {
+    if (letter !== ' ') {
+      if (results[letter])
+        results[letter] += 1;
+      else
+        results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (console.log(`✅✅✅  ${actual} === ${expected}`));
+  if (actual !== expected)
+    return (console.log(`❌❌❌ ${actual} !== ${expected}`));
+};
+
+const countBook = countLetters("lighthouse in the house");
+
+assertEqual(countBook["h"], 4);
+assertEqual(countBook["z"], undefined);
+assertEqual(countBook["n"], 1);
