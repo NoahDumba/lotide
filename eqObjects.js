42c8296ace819e0ad104bf80ed92ddb7bfeded7e
Noah Dumba <noahdumba1@gmail.com>
2020-01-08 20:55:45 +0000

    checks if two objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,45 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length)
+    return false;
+  for (key in object1) {
+    if (typeof(object1[key]) === 'object') {
+      if (!eqArrays(object1[key], object2[key]))
+        return false;
+    }
+    else if (object1[key] !== object2[key])
+      return false;
+  }
+  return true;
+};
+
+const eqArrays = function(manta, manta2) {
+  if (manta.length !== manta2.length)
+    return false;
+
+  for (let i = 0; i < manta.length; i++) {
+    if (manta[i] !== manta2[i])
+      return false;
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    return (console.log(`✅✅✅  ${actual} === ${expected}`));
+  if (actual !== expected)
+    return (console.log(`❌❌❌ ${actual} !== ${expected}`));
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
