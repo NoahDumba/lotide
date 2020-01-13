6cb1992070bb8b823389353779595398cee8d425
Noah Dumba <noahdumba1@gmail.com>
2020-01-13 17:30:37 +0000

    dryed up middle.js

--- middle.js
+++ middle.js
@@ -15,20 +15,4 @@ const middle = function(manta) {
   return newManta;
 };
 
-const assertArraysEqual = function(manta, manta2) {
-  let success = `✅✅✅  [${manta}] === [${manta2}]`;
-  let failure = `❌❌❌ [${manta}] !== [${manta2}]`;
-
-  if(manta.length !== manta2.length)
-    return console.log(failure);
-
-  for (let i = 0; i < manta.length; i++) {
-    if (manta[i] !== manta2[i])
-      return console.log(failure);
-  }
-  return console.log(success)
-};
-
-
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); //should be true
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]); //should be true
+module.exports = middle;
