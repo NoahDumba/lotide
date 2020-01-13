ce830fadac84c16085530431dc3ae3777891402c
Noah Dumba <noahdumba1@gmail.com>
2020-01-13 17:10:13 +0000

    dryed up my eqArrays code

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(manta, manta2) {
   if(manta.length !== manta2.length)
     return false
@@ -14,18 +16,4 @@ const eqArrays = function(manta, manta2) {
   return true
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return (console.log(`✅✅✅  ${actual} === ${expected}`));
-  if (actual !== expected)
-    return (console.log(`❌❌❌ ${actual} !== ${expected}`));
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+module.exports = eqArrays;
